6964fc3b72295ac915dd8223c0fe527b5dfef7eb
jorgevalerio <jorgevalerio921@gmail.com>
2017-05-03 23:35:43 -0700

    Peer Review Ready

--- js/script.js
+++ js/script.js
@@ -1,36 +1,29 @@
+// Declaring Global Variables
+
 let itemsDisplayedPerPage = 10;
 let listWrapper = document.getElementById('ListWrapper');
-let listItems = document.getElementsByTagName("li");
+let listItems = listWrapper.getElementsByTagName("li");
 let totalListItems = listItems.length;
 let totalPages = Math.ceil(totalListItems / itemsDisplayedPerPage)
 let paginationContainer = document.getElementsByClassName("pagination")[0];
-
 let paginationLink = '<ul id="listLinks">';
 
 
 window.onload = function(){
-	
-	
-	
-	for(i = 0; i < totalListItems; i+=1){
-		
-		listItems[i].style.display = 'none';
-	
-	}
-	
 
-	for(i = 0; i < itemsDisplayedPerPage; i+=1){
-		
-		listItems[i].style.display = 'block';
-	
+	// This for loop hides all li items
+	for(i = 0; i < totalListItems; i+=1){
+		listItems[i].style.display = 'none';
 	}
 	
+	// This loop displays li items according to the ItemsDisplayedPerPage
+	for(i = 0; i < itemsDisplayedPerPage; i+=1){
+		listItems[i].style.display = 'block';
+	}
 	
+	// for loop - WIll generate page links acccording to the number needed to browse all Li items
 	for(i=1;i <= totalPages; i++ ){
-		
 		paginationLink += '<li> <a href="#">'+ i +'</a>  </li>';
-		
-		
 	}
 	
 	paginationLink += '</ul>';
@@ -39,65 +32,47 @@ window.onload = function(){
 	
 	paginationContainer.innerHTML = paginationLink;
 	
+	//once page links have created we select all a inside the div listLinks
 	let liLinks = document.getElementById("listLinks").querySelectorAll('a');
-	console.log(liLinks);
 	
 	
-	
-	
-	
-
-	
-
 	
 	for(let i=0; i < liLinks.length; i++){
-		liLinks[i].className= "";
-			
 		
+							
+		// Event listener on click				
 		liLinks[i].addEventListener('click', function() {
 			
-			let yu = i;
-			console.log(yu);
-	
-		console.log('you clicked ' + i);
+			// this line will basically get the number inside the clicked object
+			let yu = this.textContent;
+			// and since array start from 0 we need to deduct 1 in order to target the right item
+			yu = yu -1;
 			
-			for(i = 0; i < totalListItems; i+=1){
-		
-		listItems[i].style.display = 'none';
-	
-	}
-			
-			///
-			
-			
-			for(i = yu * itemsDisplayedPerPage; i < (itemsDisplayedPerPage * yu) + itemsDisplayedPerPage; i+=1){
-		
-		listItems[i].style.display = 'block';
-	
-						
-			
-			///
+			//this for Loop removes any class given to liLinks
+			for(let i=0; i < liLinks.length; i++){
+			liLinks[i].className = "";
 			}
-		
-			paginationLink += '</ul>';
-			
-	});
-		
-		
-		
-			
+
+			// this line sets the class name active to the clicked a element	
+			liLinks[yu].className = "active";
+
+	
+			// This For Loop hides again all items
+			for(i = 0; i < totalListItems; i+=1){
+
+			listItems[i].style.display = 'none';
+
+			 }
+			// This loop shows only the li items according page link clicked 
+				for(i = yu * itemsDisplayedPerPage; i < (itemsDisplayedPerPage * yu) + itemsDisplayedPerPage; ++i){
+				listItems[i].style.display = 'block';
+
+				}
+
+		});		
 	}
 	
-	
-	
+// This will set the first pagination link with the class Active which will then be removed by the event listner 
 	liLinks[0].className = "active";
-	
-	
-	
-	
+		
 };
-
-
-
-
-
